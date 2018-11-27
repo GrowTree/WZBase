3b186745379d6b0d21a494df0357d75897cc6aac
GrowTree <1547020236@qq.com>
2018-11-27 14:20:44 +0800

    WZNavigationController

---
+++ WZBase/Classes/WZNavigationController.h
@@ -0,0 +1,12 @@
+//
+//  WZNavigationController.h
+//  Pods-WZBase_Example
+//
+//  Created by 饶煦庭 on 2018/11/27.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WZNavigationController : UINavigationController
+
+@end
---
+++ WZBase/Classes/WZNavigationController.m
@@ -0,0 +1,22 @@
+//
+//  WZNavigationController.m
+//  Pods-WZBase_Example
+//
+//  Created by 饶煦庭 on 2018/11/27.
+//
+
+#import "WZNavigationController.h"
+
+@interface WZNavigationController ()
+
+@end
+
+@implementation WZNavigationController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+}
+
+
+@end
